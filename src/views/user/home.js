aaecb5f27c3c69e090c5f48b4bf51f9f75875bb2
Mugdha Shah <shahmugdha15@gmail.com>
2021-10-18 23:37:51 +0530

    wishlist icon dual functions done

--- src/views/user/home.js
+++ src/views/user/home.js
@@ -6,6 +6,7 @@ import Footer from "../../components/footer.js";
 import { getProductList, getParticularProduct, selectProduct } from "../../redux/actions/products/index.js";
 import { addProductToWishList, getWishList, deleteProductFromWishList } from "../../redux/actions/wishlist/index.js";
 import { addProductToCart } from "../../redux/actions/cart/index.js";
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
 import { toast } from 'react-toastify'
 import "../../components/products.css"
@@ -13,23 +14,42 @@ import "../../components/Navigation/sideNav.css"
 import "../../components/addToCartPopUp.css"
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
+
 const Home = () => {
   const [size, setSize] = useState("Small")
-  const [isWishlisted, SetIsWishlisted] = useState(false)
   const productData = useSelector(state => state.products.productData)
   const particularProduct = useSelector(state => state.products.particularProduct)
   const key = 'title';
   const productsUniqueByKey = [...new Map(productData.map(item => [item[key], item])).values()];
   const wishlistData = useSelector(state => state.wishlist.wishlistData)
-  let wishlistedProducts = new Object()
+  let wishlistedProducts = []
+  let objToAdd = {}
+  var foundProduct
+
   if(wishlistData && wishlistData[0] && wishlistData[0].products) {
-    wishlistedProducts.productId = wishlistData[0].products
-    wishlistedProducts.wishlisted = true
+    productData.map(prod => {
+      wishlistData[0].products.map(wishProd => {
+        if(wishProd._id === prod._id) {
+          objToAdd = {productId: prod._id, wishlisted: true}
+          wishlistedProducts = [...wishlistedProducts, objToAdd]
+        }
+      })
+    })
   }
+
   const markWishlisted = (productId) => {
-    wishlistedProducts.push({productId, wishlisted: true})
+    objToAdd = {productId, wishlisted: true}
+    wishlistedProducts = [...wishlistedProducts, objToAdd]
   }
-  console.log(wishlistedProducts, "wihsprod ob")
+
+  const unMarkWishlisted = (productId) => {
+    for(var i = 0; i < wishlistedProducts.length; i++) {
+      if(wishlistedProducts[i].productId == productId) {
+        wishlistedProducts.splice(i, 1);
+        break;
+      }
+    }
+  }
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getProductList())
@@ -41,14 +61,24 @@ const Home = () => {
   }
 
   const addRemoveWishlist = (productId) => {
-    SetIsWishlisted(!isWishlisted)
-    if(isWishlisted === true) {
+    wishlistedProducts.forEach(prod => {
+      if(prod.productId === productId) {
+        foundProduct = productId
+      }
+    }) 
+    if(foundProduct !== productId) {
+      console.log(foundProduct, "add")
+      markWishlisted(productId)
       dispatch(addProductToWishList(productId))
     }
-    else if (isWishlisted === false) {
+    else if(foundProduct === productId) {
+      console.log(foundProduct, "delete")
+      unMarkWishlisted(productId)
       dispatch(deleteProductFromWishList(productId))
-    }   
+    }
+    window.location.reload()
   }
+  console.log(wishlistedProducts, "wihsprod ob")
 
   return(
     <>
@@ -60,12 +90,32 @@ const Home = () => {
           {productsUniqueByKey.map((product) => {
             return (
               <figure>
-                <Link to = {`/collection/product/${product._id}`}><img style={{height: "300px"}} src={`http://localhost:5000/${product.image}`}/></Link>
+                <Link to = {`/collection/product/${product._id}`}>
+                  <img style={{height: "300px"}} src={`http://localhost:5000/${product.image}`}/>
+                </Link>
+
                 <figcaption>
                   {product.title} 
-                  <FavoriteBorderOutlinedIcon style={{float: "right", fontSize: "23px"}} onClick={() => addRemoveWishlist(product._id)}
-                    
-                  />
+                  {
+                    wishlistedProducts.forEach(prod => {
+                      if(prod.productId === product._id) {
+                        foundProduct = prod.productId
+                      }
+                    }) 
+                  }
+                  {
+                    foundProduct === product._id? 
+                      <FavoriteIcon 
+                        style={{float: "right", fontSize: "23px", color: "#f40d30", cursor: "pointer"}} 
+                        onClick={() => addRemoveWishlist(product._id)}
+                      />
+                    : 
+                    <FavoriteBorderOutlinedIcon
+                      style={{float: "right", fontSize: "23px", cursor: "pointer"}} 
+                      onClick={() => addRemoveWishlist(product._id)}
+                    />
+                  }
+                  
                 </figcaption>
                 <div style={{float: "left", color: "GrayText"}}>{product.description}</div><br/>
                 <span style={{float: "left", height: "35px"}} className="price">Rs.{product.price}</span>
